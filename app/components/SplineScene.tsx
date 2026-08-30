7412d7fcc03591e30b70b35ffbc7b49b3ee05ba1
Soumyadip Sil <soumyadipsil689@gmail.com>
2026-08-31 02:12:08 +0530

    Add missing SplineScene component

---
+++ app/components/SplineScene.tsx
@@ -0,0 +1,13 @@
+export default function SplineScene() {
+  return (
+    <div className="absolute inset-0 w-full h-full z-0 opacity-40 mix-blend-screen pointer-events-none overflow-hidden flex items-center justify-center">
+      <iframe 
+        src="https://my.spline.design/aigreymarketingbanner-3CeqWCxMZOe4jIeBrHhliCVC/" 
+        frameBorder="0" 
+        width="100%" 
+        height="100%"
+        className="w-[120%] h-[120%] object-cover scale-110"
+      ></iframe>
+    </div>
+  );
+}
